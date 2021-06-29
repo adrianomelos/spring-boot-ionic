cf16e35271257994b3159359913d35cf2e29a917
Adriano Melo <adrianoshout@gmail.com>
2021-06-29 08:43:27 -0300

    adicionando o banco de dados h2

---
+++ src/main/java/com/ionic/api/entity/Categoria.java
@@ -0,0 +1,31 @@
+package com.ionic.api.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String categoria;
+
+}

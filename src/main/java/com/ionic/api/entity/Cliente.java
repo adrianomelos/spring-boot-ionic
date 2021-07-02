53c27c9221aca61b0e2b882fbe7e45a85a41a3f4
Adriano Melo <adrianoshout@gmail.com>
2021-07-02 14:08:09 -0300

    módulos de clientes

--- src/main/java/com/ionic/api/entity/Cliente.java
+++ src/main/java/com/ionic/api/entity/Cliente.java
@@ -1,14 +1,12 @@
 package com.ionic.api.entity;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -21,16 +19,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "tb_estado")
-public class Estado implements Serializable {
+@Table(name = "tb_cliente")
+public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
-	
-	@OneToMany
-	private List<Cidade> cidades = new ArrayList<>();
 
 }
---
+++ src/main/java/com/ionic/api/entity/TipoCliente.java
@@ -0,0 +1,31 @@
+package com.ionic.api.entity;
+
+public enum TipoCliente {
+
+	PF(0),
+	PJ(1);
+
+	private int cod;
+	
+	private TipoCliente(int i) {
+		this.cod = i;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+	
+	public static TipoCliente toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(TipoCliente t : TipoCliente.values()) {
+			if(cod.equals(t.getCod())) {
+				return t;
+			}
+		}
+		
+		throw new IllegalArgumentException("id inválido" + cod);
+	}
+}

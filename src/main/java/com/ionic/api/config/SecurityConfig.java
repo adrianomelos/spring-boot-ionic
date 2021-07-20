e688107d5bad6ffb3c075c760224a1487daf9ecf
Adriano Melo <adrianoshout@gmail.com>
2021-07-19 21:16:55 -0300

    implementando o jwt autorização

--- src/main/java/com/ionic/api/config/SecurityConfig.java
+++ src/main/java/com/ionic/api/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ionic.api.security.JWTAuthenticationFilter;
+import com.ionic.api.security.JWTAuthorizationFilter;
 import com.ionic.api.security.JWTUtil;
 
 @Configuration
@@ -65,6 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
